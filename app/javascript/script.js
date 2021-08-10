2c3824d45cd905021b3203d237269b1ec6da7909
root-344 <michimasa8@gmail.com>
2021-08-10 17:36:49 +0900

    modal作成

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,12 @@
 $(function() {
-
+  $('.sign-up-btn').click(function() {
+    $('#signup-modal').fadeIn(0);
+  });
+  $('.login-btn').click(function() {
+    $('#login-modal').fadeIn(0);
+  });
+  $('.close-modal').click(function() {
+    $('#signup-modal').fadeOut(0);
+    $('#login-modal').fadeOut(0);
+  });
 });
